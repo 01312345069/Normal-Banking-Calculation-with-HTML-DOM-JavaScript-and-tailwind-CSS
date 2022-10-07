7ef26c20c9aa87a6f4acf46bf0c77114d55aac14
Md Naymul Islam মোঃ নয়িমুল ইসলাম <mybspi@gmail.com>
2022-10-07 15:32:17 +0600

    fix all bug and minor change

--- withdraw.js
+++ withdraw.js
@@ -20,28 +20,45 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawField = document.getElementById('withdraw-field');
     const newWithDrawAmountString = withdrawField.value;
     const newWithDrawAmount = parseFloat(newWithDrawAmountString);
-    console.log(newWithDrawAmount);
+    // console.log(newWithDrawAmount);
+
+
+    // step-7: 
+    withdrawField.value = '';
+
+    if (isNaN(newWithDrawAmount)) {
+        alert('Please type Valid number');
+        return;
+    }
 
     // step-3:
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    // step-4 
-    const currentWithdrawTotal = previousWithdrawTotal + newWithDrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
+
 
     // step-5:
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+
+
+
+    if (newWithDrawAmount > previousBalanceTotal) {
+        alert('Your Father Bank has Not Enough money ');
+        return;
+    }
+
+    // step-4 
+    const currentWithdrawTotal = previousWithdrawTotal + newWithDrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
     // step-6:
     const newBalanceTotal = previousBalanceTotal - newWithDrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
 
-    // step-7: 
-    withdrawField.value = '';
 
 
 
