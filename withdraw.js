02f0b915e7a17c9202c7c4bc460bf9fa22d5c0f3
Md Naymul Islam মোঃ নয়িমুল ইসলাম <mybspi@gmail.com>
2022-10-07 14:53:57 +0600

    add withdraw function

--- withdraw.js
+++ withdraw.js
@@ -1,14 +1,21 @@
 /*
 1: add event handler with the withdraw button 
-2. get the windraw amount from the withdraw input field
+2. get the windraw amount from the withdraw input field.
+2.5 alson make sure to convert the input into a number by usuing parseFloat.
+3. get previous withdrow total.
 
 */
 
 // step-1
 document.getElementById('btn-withdraw').addEventListener('click', function () {
-
+    // step-2:
     const withdrawField = document.getElementById('withdraw-field');
     const newWithDrawAmountString = withdrawField.value;
     const newWithDrawAmount = parseFloat(newWithDrawAmountString);
     console.log(newWithDrawAmount);
+
+    // step-3:
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+
 })
